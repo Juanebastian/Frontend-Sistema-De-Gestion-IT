5d943380f745694e5c009daf2e1a7e3a92a1a450
JUAN SEBASTIAN DIAZ CHAPARRO <juansebastiandiazchaparro27@gmail.com>
2025-05-19 23:20:34 -0500

    adicion componente login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './features/auth/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full' }, // 👈 Ruta por defecto
+    
+    { path: 'login', component: LoginComponent }
+];
---
+++ src/app/features/admin/administrador-layout/administrador-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-administrador-layout',
+  imports: [],
+  templateUrl: './administrador-layout.component.html',
+  styleUrl: './administrador-layout.component.scss'
+})
+export class AdministradorLayoutComponent {
+
+}
